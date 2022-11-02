c399d6ee76503f95a3f1238a305594191e53ae5b
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-11-02 13:59:29 -0600

    Add Contact section

--- script.js
+++ script.js
@@ -17,3 +17,36 @@
     $menu.classList.remove("is-active");
   });
 })(document);
+
+// Contact form
+((d) => {
+  const $form = d.querySelector(".contact-form");
+  const $loader = d.querySelector(".contact-form-loader");
+  const $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/info@amauridev.me", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#thanks";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.error(err);
+        let msg = err.statusText || "Ops! an error occurred, try again";
+        $response.querySelector("h3").innerHTML = `Error ${err.status} ${msg}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 2500);
+      });
+  });
+})(document);
